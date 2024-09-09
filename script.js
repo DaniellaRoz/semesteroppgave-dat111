e01ffc1a10c0bf512c240f1c3698b4116bc8f859
Daniella Stensland <dellasten25@gmail.com>
2024-09-09 11:29:50 +0200

    første iterasjon av drop-down menyen, mer justeringer trengs får å gjøre den perfect

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var toggled = false;
+
+    const dropDownButton = document.querySelector(".drop-down-button");
+    const dropDownContent = document.querySelector(".drop-down-content");
+
+    function adjustButtonPosition() {
+        // Adjust button position if needed
+        const contentHeight = dropDownContent.scrollHeight;
+        
+        toggled ? dropDownButton.style.top = `${contentHeight + 90}px` : dropDownButton.style.top = `${90}px`
+    }
+
+    dropDownButton.addEventListener("click", function() {
+        toggled = !toggled;
+        dropDownContent.classList.toggle("show");
+        dropDownButton.classList.toggle("rotate");
+        adjustButtonPosition();
+    });
+
+    adjustButtonPosition(); // For adjusting the position on page load
+});
